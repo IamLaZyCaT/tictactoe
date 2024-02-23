c78dd39b3c8efc0ddc17752f07a823f21d5d472c
IamLaZyCaT <ashishkumal89@gmail.com>
2024-02-23 23:48:54 +0545

    hello

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+import TicTacToe from "./TicTacToe/TicTacToe";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Tic Tac Toe in react</h1>
+      <TicTacToe />
+    </div>
+  );
+}
